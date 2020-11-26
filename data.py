3357f1a231a624a96cfa59706cd068d9ab9e04b3
Lorris Giovagnoli <lorris.giovagnoli@outlook.fr>
2020-11-26 19:59:49 +0100

    Create data module to load data and compute descriptors

---
+++ data.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from os.path import join
+import copy
+import pickle
+import numpy as np
+from ase import Atoms
+from dscribe.descriptors import SOAP
+
+data_path='./data'
+
+datasets = {
+	'zundel': {
+		'atoms': [8, 8, 1, 1, 1, 1, 1],
+		'data': {
+			'pos': join(data_path, 'zundel_100K_pos'),
+			'energies': join(data_path, 'zundel_100K_energy')
+		},
+		'soap': {
+			'species': [1, 8],
+			'periodic': False,
+			'sigma': 0.01,
+			'nmax': 3,
+			'lmax': 2,
+			'rcut': 5
+		}
+	}
+}
+
+def load(dataset='zundel', limit=None, soap_params=None):
+	"""
+	Load a specific dataset and compute descriptors
+
+	params:
+		- dataset: the name of the dataset
+		- limit: reduce the size of the dataset
+		- soap_params: an object containing custom soap parameters (some are set by default)
+	"""
+
+	params = copy.deepcopy(datasets[dataset])
+	if soap_params != None:
+		params['soap'].update(soap_params)
+
+	pos = pickle.load(open(params['data']['pos'], 'rb'))
+	energies = pickle.load(open(params['data']['energies'], 'rb'))
+
+	if limit != None:
+		pos, energies = pos[:limit], energies[:limit]
+
+	tot_time = np.shape(pos)[0]
+	molecs = np.empty(tot_time, dtype=object)
+	for t in range(tot_time):
+		molecs[t] = Atoms(params['atoms'], positions=pos[t])
+
+	soap = SOAP(**params['soap'])
+	descriptors = soap.create(molecs)
+
+	return descriptors, energies
+
--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
+
+#import matplotlib.pyplot as plt
+#import tensorflow as tf
+#import keras, sklearn, pickle
+#from dscribe.descriptors import SOAP
+#from ase.io import read
+#from ase.build import molecule
+#from ase import Atoms
+
+import data
 import numpy as np
-import keras,sklearn,pickle
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from dscribe.descriptors import SOAP
-from ase.io import read
-from ase.build import molecule
-from ase import Atoms
 
-#SOAP parameters
-species = ["H", "O"]
-#sigma=
-nmax = 3
-lmax = 8
-rcut = 5
+soap_params = {
+	'sigma': 0.01,
+	'nmax': 3, 
+	'lmax': 3,
+	'rcut': 5
+}
 
-#Configure SOAP descriptor
-soap = SOAP(
-    species=species,
-    periodic=False,
-    rcut=rcut,
-    nmax=nmax,
-    lmax=lmax,
-    average="outer",
-    sparse=False
-)
+# Load dataset and compute descriptors
+descriptors, energies = data.load(
+	dataset = 'zundel', soap_params=soap_params, limit=1000)
+
+print(np.shape(descriptors))
+print(np.shape(energies))
 
 #Create NN: input = descriptors, output = system's total energy
 
 #Step 1: process data
 
 #Import data
-energies = pickle.load(open('./data/zundel_100K_energy', 'rb'))
-pos = pickle.load(open('./data/zundel_100K_pos', 'rb'))
 
 #Take fraction of the data
 
@@ -40,42 +37,42 @@ pos = pickle.load(open('./data/zundel_100K_pos', 'rb'))
 
 #Step 2: Define NN and architecture
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-
-def create_DNN():
-    # instantiate model
-    model = Sequential()    
-    return model
-
-#Step 3: Optimizer and cost function
-
-def compile_model(optimizer=keras.optimizers.Adam()):
-    # create the mode
-    model=create_DNN()
-    # compile the model
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=optimizer,
-                  metrics=['accuracy'])
-    return model
-
-#Step 4: Train the model
-    
-# training parameters
-batch_size = 64
-epochs = 10
-
-# create the deep neural net
-model_DNN=compile_model()
-
-# train DNN and store training info in history
-history=model_DNN.fit(descriptors_train, energies_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(descriptors_test, energies_test))
-
-#Step 5: evaluate performance
-
-#Step 6: Optimize
+#from keras.models import Sequential
+#from keras.layers import Dense, Dropout, Flatten
+#from keras.layers import Conv2D, MaxPooling2D
+#
+#def create_DNN():
+#    # instantiate model
+#    model = Sequential()    
+#    return model
+#
+##Step 3: Optimizer and cost function
+#
+#def compile_model(optimizer=keras.optimizers.Adam()):
+#    # create the mode
+#    model=create_DNN()
+#    # compile the model
+#    model.compile(loss=keras.losses.categorical_crossentropy,
+#                  optimizer=optimizer,
+#                  metrics=['accuracy'])
+#    return model
+#
+##Step 4: Train the model
+#    
+## training parameters
+#batch_size = 64
+#epochs = 10
+#
+## create the deep neural net
+#model_DNN=compile_model()
+#
+## train DNN and store training info in history
+#history=model_DNN.fit(descriptors_train, energies_train,
+#          batch_size=batch_size,
+#          epochs=epochs,
+#          verbose=1,
+#          validation_data=(descriptors_test, energies_test))
+#
+##Step 5: evaluate performance
+#
+##Step 6: Optimize
